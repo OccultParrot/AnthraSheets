8988bdb9a70f78791acabfac8d70879705373e48
Thomas Stemler <stemlertho@gmail.com>
2025-08-01 23:15:44 -0500

    Added more options for the form, still more to go

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,104 +1,70 @@
-import { useState } from "react";
+import {useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import TextInput from "../inputs/TextInput.tsx";
+import ListInput from "../inputs/ListInput.tsx";
+import type {InputChangeEvent} from "../../config/types.ts";
 
-interface FormOptionProps {
-  label: string;
+interface FormState {
   name: string;
-  type: string;
-  required: boolean;
-}
-
-function FormOption({ label, name, type, required }: FormOptionProps) {
-  return (
-    <div className="mb-4 flex flex-row items-center">
-      <label className="block text-sm font-medium text-1 mb-2">{ label }:</label>
-      <input
-        name={ name }
-        type={ type }
-        required={ required }
-        className="h-8 ml-2 w-fit p-2 bg-3 rounded-md focus:outline-none focus:ring-2 focus:border-transparent"
-      />
-    </div>
-  );
+  traits: string[];
+  description?: string;
+  species: string;
+  subspecies: string;
+  gender: string;
+  dominantSkin: string;
+  recessiveSkins: string[];
+  eyeColor: string;
+  mutations: string[];
 }
 
 function HomePage() {
-  const [ formData, setFormData ] = useState({
+  
+  const copyToClipboard = (text: string) => {
+    if (navigator.clipboard) {
+      try {
+        navigator.clipboard.writeText(text).then(() => {
+          alert("Copied to clipboard!")
+        });
+      } catch (err) {
+        console.error("Failed to copy: ", err);
+      }
+    } else {
+      alert("Clipboard API not supported.");
+    }
+  }
+
+  const [ formData, setFormData ] = useState<FormState>({
     name: '',
     traits: [],
-    genetics: {
-      species: '',
-      subSpecies: '',
-      gender: '',
-      age: 0,
-      immuneSystem: '',
-      status: ''
-    },
-    milestones: [],
-    appearance: {
-      dominantSkin: '',
-      recessiveSkin: '',
-      eyeColor: '',
-      mutations: []
-    },
-    family: {
-      father: {
-        name: '',
-        dominantSkin: '',
-        recessiveSkin: '',
-        eyeColor: '',
-        subspecies: '',
-        immuneSystem: '',
-        traits: [],
-        mutations: []
-      },
-      mother: {
-        name: '',
-        dominantSkin: '',
-        recessiveSkin: '',
-        eyeColor: '',
-        subspecies: '',
-        immuneSystem: '',
-        traits: [],
-        mutations: []
-      },
-      siblings: [],
-      offspring: []
-    }
+    species: '',
+    subspecies: '',
+    gender: '',
+    dominantSkin: '',
+    recessiveSkins: [],
+    eyeColor: '',
+    mutations: [],
   });
 
-  return (
-    <div className="bg-4 text-1 rounded-2xl p-4 m-4 flex flex-row justify-between">
-      <div className="py-4 px-6">
-        <h2 className="text-xl font-bold mb-4">Character Form</h2>
-        <FormOption label="Name" name="name" type="text" required/>
-        <FormOption label="Species" name="species" type="text" required/>
-        <FormOption label="Subspecies" name="subspecies" type="text" required/>
-        <FormOption label="Gender" name="gender" type="text" required/>
-        <FormOption label="Age" name="age" type="number" required/>
-        <FormOption label="Immune System" name="immuneSystem" type="text" required/>
-        <FormOption label="Status" name="status" type="text" required/>
-      </div>
-      <div className="bg-5 p-4 rounded-lg w-1/2">
-        <div className="flex items-center justify-between mb-4 px-2">
-          <h2 className="text-xl font-bold mb-4">Preview</h2>
-          <button className="p-2 rounded-xl hover:bg-3 transition ease-in-out duration-250" onClick={() => alert("Copied to clipboard!")}>
-            <ContentCopyIcon/>
-          </button>
-        </div>
-        <article className="prose prose-sm prose-invert px-2">
-          <Markdown remarkPlugins={[remarkGfm]}>{ `
-# NAME
+  const onChange = (e: InputChangeEvent<any>) => {
+    const { name, value } = e;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+  
+  const markdownContent = `
+# ${formData.name.trim()}
 
-*trait, trait, trait*
+*${formData.traits.join(', ')}*
 
-*meaning of name*
+${formData.description ? `*${formData.description.trim()}*` : ''}
 
 \`Genetic Information\`
-> - **Species & Variant**: 
-> - **Gender**: 
+> - **Species & Variant**: *${formData.species} ${formData.subspecies}*
+> - **Gender**: ${formData.gender}
 > - **Age**: XX season(s)
 > - **Immune System Type**: N(eutral) / W(eak) / S(trong)
 > - **Status**: alive/dead
@@ -113,10 +79,10 @@ function HomePage() {
 > :DIAMONDMEDAL: 
 
 \`Appearance\`
-> - **d**ominant skin: 
-> - **r**ecessive skin(s): 
-> - **eye-colour**: 
-> - **mutations**: 
+> - **d**ominant skin: ${formData.dominantSkin}
+> - **r**ecessive skin(s): ${formData.recessiveSkins}
+> - **eye-colour**: ${formData.eyeColor}
+> - **mutations**: ${formData.mutations.length > 0 ? formData.mutations.join(', ') : 'none'}
 
 \`Family Tree\`   
 > ♂ **father**: *NAME*
@@ -149,8 +115,38 @@ function HomePage() {
 > - **sire/dame**: [link to sheet]
 > - :egg: • [name] | [gender] | [status]
 > - :egg: • [name] | [gender] | [status]
-> - :egg: • [name] | [gender] | [status]
-          ` }</Markdown>
+> - :egg: • [name] | [gender] | [status]`
+
+  return (
+    <div className="bg-4 text-1 rounded-2xl p-4 m-4 flex flex-col md:flex-row md:justify-between ">
+      <div className="py-4 px-6 w-auto md:w-1/2">
+        <h2 className="text-xl font-bold mb-4">Character Form</h2>
+        
+        <h3 className="text-lg font-bold mb-2 text-center">── General ──</h3>
+        <TextInput label="Name" name="name" onChange={onChange}  />
+        <TextInput label="Description" placeholder="Optional" name={"description"} onChange={onChange} />
+        <ListInput label="Traits" name="traits" onChange={onChange} />
+
+        <h3 className="text-lg font-bold mb-2 text-center">── Genetics ──</h3>
+        <TextInput label="Species" name="species" onChange={onChange} />
+        <TextInput label="Subspecies" name="subspecies" onChange={onChange} />
+        <TextInput label="Gender" name="gender" onChange={onChange} />
+        <TextInput label="Dominant Skin" name="dominantSkin" onChange={onChange} />
+        <ListInput label="Recessive Skin(s)" name="recessiveSkins" onChange={onChange} />
+        <TextInput label="Eye Color" name="eyeColor" onChange={onChange} />
+        <ListInput label="Mutations" name="mutations" onChange={onChange} />
+        
+      </div>
+      <div className="bg-5 p-4 rounded-lg w-auto md:w-1/2">
+        <div className="flex items-center justify-between mb-4 px-2">
+          <h2 className="text-xl font-bold mb-4">Preview</h2>
+          <button className="p-2 rounded-xl hover:bg-3 transition ease-in-out duration-250"
+                  onClick={ () => copyToClipboard(markdownContent) }>
+            <ContentCopyIcon/>
+          </button>
+        </div>
+        <article className="prose prose-sm prose-invert px-2">
+          <Markdown remarkPlugins={ [ remarkGfm ] }>{markdownContent}</Markdown>
         </article>
       </div>
     </div>
