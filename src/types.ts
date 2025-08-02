2ebd375b294bda6386bf58177b29c9665b570d92
Thomas Stemler <stemlertho@gmail.com>
2025-08-02 16:03:25 -0500

    Added types and updated types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import type { ChangeEvent, ReactElement, ReactNode } from "react";
+import type { ReactElement, ReactNode } from "react";
 
 export type RouteProps = {
   name: string;
@@ -36,4 +36,64 @@ export interface TextInputProps extends FormOptionProps {
 
 export interface ListInputProps extends FormOptionProps {
   maxItems?: number;
+}
+
+export interface NumberInputProps extends FormOptionProps {
+  min?: number;
+  max?: number;
+  step?: number;
+}
+
+export interface Clutchmate {
+  name: string;
+  link?: string;
+  sex: string;
+  
+}
+
+export interface Clutch {
+  partner: string;
+  partnerLink?: string;
+  eggCount: number;
+  eggs: Egg[];
+}
+
+export interface Egg {
+  name: string;
+  link?: string;
+  gender: string;
+  status: string;
+}
+
+export interface FormState {
+  name: string;
+  age: number;
+  traits: string[];
+  description?: string;
+  status: string;
+  bronzeMileStone: string;
+  silverMileStone: string;
+  goldMileStone: string;
+  diamondMileStone: string;
+  species: string;
+  subspecies: string;
+  gender: string;
+  dominantSkin: string;
+  recessiveSkins: string[];
+  eyeColor: string;
+  mutations: string[];
+  fatherName: string;
+  fatherLink?: string;
+  fatherDominantSkin: string;
+  fatherRecessiveSkins: string[];
+  fatherEyeColor: string;
+  fatherHealthGenesMutations: string;
+  motherName: string;
+  motherLink?: string;
+  motherDominantSkin: string;
+  motherRecessiveSkins: string[];
+  motherEyeColor: string;
+  motherHealthGenesMutations: string;
+  clutchmates: Clutchmate[];
+  clutches: Clutch[];
 }
