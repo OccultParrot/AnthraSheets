f8fa6784d5ebc03b9a2b644b273851c941ad674b
Thomas Stemler <stemlertho@gmail.com>
2025-09-16 21:03:57 -0500

    fixed foolish error:

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,7 +1,7 @@
 import { backgroundPhotos } from "../../config/photos.tsx";
 import Discord from "/discord.svg"
 import type { BackgroundPhoto } from "../../types.ts";
-import { type MouseEventHandler, useState } from "react";
+import { useState } from "react";
 
 function getRandomPhoto(): BackgroundPhoto {
   const photoIndex = Math.floor(Math.random() * backgroundPhotos.length);
@@ -13,12 +13,12 @@ function getRandomPhoto(): BackgroundPhoto {
 }
 
 function LandingPage() {
-  const [photo, setPhoto] = useState<BackgroundPhoto>(getRandomPhoto())
-  
+  const [ photo, setPhoto ] = useState<BackgroundPhoto>(getRandomPhoto())
+
   const changePhoto = () => {
     setPhoto(getRandomPhoto());
   }
-  
+
   console.log(photo);
 
   return (
@@ -30,17 +30,20 @@ function LandingPage() {
         <h1 className="mt-8 text-6xl md:text-8xl font-header">AnthraSheets</h1>
         <p className="mt-4 text-2xl font-sans">Species Directory</p>
         <div className="mt-18 flex flex-row gap-8">
-          <button className="p-4 w-fit text-md bg-blurple rounded-2xl hover:bg-dark-blurple transition-colors flex flex-row gap-4 justify-center items-center cursor-pointer select-none">
-            <img src={Discord} alt="Discord Icon" className="h-8" />
+          <button
+            className="p-4 w-fit text-md bg-blurple rounded-2xl hover:bg-dark-blurple transition-colors flex flex-row gap-4 justify-center items-center cursor-pointer select-none">
+            <img src={ Discord } alt="Discord Icon" className="h-8"/>
             Login with Discord
           </button>
         </div>
       </div>
       <div className="flex flex-col md:flex-row w-full justify-between fixed bottom-0 items-center">
-        <button onClick={changePhoto} className=" flex flex-row gap-4  m-4 text-lg bg-slate-600/20 hover:bg-slate-800/20 transition-colors rounded-2xl p-4 backdrop-blur-md cursor-pointer select-none">
+        <button onClick={ changePhoto }
+                className=" flex flex-row gap-4  m-4 text-lg bg-slate-600/20 hover:bg-slate-800/20 transition-colors rounded-2xl p-4 backdrop-blur-md cursor-pointer select-none">
           Switch Background
         </button>
-        <div className=" flex flex-row gap-4 w-fit  m-4 text-lg bg-slate-600/20 rounded-2xl p-4 backdrop-blur-md cursor-default select-none">
+        <div
+          className=" flex flex-row gap-4 w-fit  m-4 text-lg bg-slate-600/20 rounded-2xl p-4 backdrop-blur-md cursor-default select-none">
           <p>
             Photo by { photo.photographer }
           </p>
@@ -49,7 +52,6 @@ function LandingPage() {
     </div>
   )
 }
-
 
 
 export default LandingPage;
